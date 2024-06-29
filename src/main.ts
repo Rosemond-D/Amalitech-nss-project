4bf23e0e95fef64b19542eb0ab51a827056b4f53
Rosemond-D <rosemondetornam@gmail.com>
2024-06-29 05:49:28 -0700

    added a feature to change theme from light to dark on the page

--- src/main.ts
+++ src/main.ts
@@ -33,3 +33,51 @@ import DOMPurify from 'dompurify';
         
     });
 })();
+
+/**
+ * This section of the code handles the theme switching functionality.
+ * It adds an event listener for 'change' event on the toggleSwitch.
+ */
+const toggleSwitch: HTMLInputElement = document.querySelector('#theme-toggle-button') as HTMLInputElement;
+const btnToggleText: HTMLSpanElement = document.querySelector('#toggle-announce') as HTMLSpanElement;
+
+/**
+ * This function switches the theme to dark.
+ * It sets a localStorage variable to track the theme between page loads.
+ */
+export const switchToDarkTheme = (): void => {
+    localStorage.setItem('theme', 'dark');
+    document.documentElement.setAttribute('data-theme', 'dark');
+    toggleSwitch.checked = true;
+    btnToggleText.innerText = "switch to light theme";
+}
+
+/**
+ * This function switches the theme to light.
+ * It sets a localStorage variable to track the theme between page loads.
+ */
+export const switchToLightTheme = (): void => {
+    localStorage.setItem('theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'light');
+    toggleSwitch.checked = false;
+    btnToggleText.innerText = "switch to dark theme";
+}
+
+/**
+ * This function switches the theme based on the checked state of the toggleSwitch.
+ * @param {Event} e - The event object.
+ */
+const switchTheme = (e: Event): void => {
+    if ((e.target as HTMLInputElement).checked) {
+        switchToDarkTheme();
+    } else {
+        switchToLightTheme();
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const mediaQueryDarkMode: MediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+if (mediaQueryDarkMode && mediaQueryDarkMode.matches) {
+    switchToDarkTheme();
+}
